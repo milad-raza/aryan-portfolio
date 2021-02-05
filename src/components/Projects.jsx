36e31d73b38904ac0827a82c9e190bf005dcc435
milad-raza <miladrazabhatti@gmail.com>
2021-02-05 20:59:33 +0500

    Minor Bugs Fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,8 +8,8 @@ export default function Projects() {
   return (
     <div className={styles.projects} id="projects">
       <h1 className={styles.heading}>Projects</h1>
+      <center>
       <div className={styles.allp}>
-
       <ProjectCard
         image={covid}
         title="COVID-19 Live Web App"
@@ -28,7 +28,9 @@ export default function Projects() {
         github="https://github.com/harsimran-preet/theextraction"
         details="The Extraction made completely in java is a strategic game where you play as a hero who's task is to kill the enemies before they get access to the secret nuclear codes. You also have performance enhancers in the game on both the good and the bad side. Your strategic deployment of heroes is the only way to stop this attack. Good Luck!"
       />
+
       </div>
+      </center>
     </div>
   );
 }
